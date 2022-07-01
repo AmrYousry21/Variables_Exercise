6213c3fdb00863da7ed7162636a918eadad5070b
johndward01 <johndward01@gmail.com>
2022-07-01 13:35:20 -0500

    Add project files.

---
+++ Variables_Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿string name = "John";
+int age = 31;
+char letter = '!';
+bool isAuthenticated = true;
+double pi = 3.14;
+decimal price = 1.99m;
+
+Console.WriteLine($"Hey, my name is {name} and I am " +
+                  $"{age} years old{letter}\n" +
+                  $"Authentication Status: {isAuthenticated}\n" +
+                  $"My Representation of Pi is: {pi}\n" +
+                  $"The price of the product is: {price}\n");
+
+
